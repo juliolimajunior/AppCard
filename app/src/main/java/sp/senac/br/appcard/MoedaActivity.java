e5eef11f42ba06e45f64203125973e554482ab21
JULIO CESAR SANTOS LIMA JUNIOR <julio.csljunior@senacsp.edu.br>
2019-08-09 12:19:04 -0300

    Initial commit

---
+++ app/src/main/java/sp/senac/br/appcard/MoedaActivity.java
@@ -0,0 +1,13 @@
+package sp.senac.br.appcard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MoedaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.moeda_layout);
+    }
+}
---
+++ app/src/main/java/sp/senac/br/appcard/MusicaActivity.java
@@ -0,0 +1,13 @@
+package sp.senac.br.appcard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MusicaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_musica);
+    }
+}
